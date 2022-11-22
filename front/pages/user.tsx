4e51bc8f3370503beb11c754a91eb3a61c8e1765
Piotr Obuchowski <pobuchowski@valuelogic.one>
2022-11-22 12:15:22 +0100

    #9: fixed redirect on auth error

--- front/pages/user.tsx
+++ front/pages/user.tsx
@@ -16,7 +16,7 @@ export async function getServerSideProps(context) {
     if (!session) {
         return {
             redirect: {
-                destination: '/user',
+                destination: '/',
                 permanent: false,
             },
         };
